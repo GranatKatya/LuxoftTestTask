9cadc8da73c402be3e012239c297cf82caa9aab3
Katya Granat <k.granat2017@.com>
2018-10-02 00:18:56 +0300

    acorrect tests

---
+++ Granat_Katya_pre-interview/fileForTests.cpp
@@ -0,0 +1,40 @@
+#include "stdafx.h"
+
+#include "ApplicationTest.h" 
+#include "FactoryTest.h" 
+#include "FileTest.h" 
+#include "IProcessorsTests.h" 
+
+int main1(int argc, char* argv[]) {
+
+	setlocale(LC_ALL, "rus"); // корректное отображение Кириллицы
+	
+	ApplicationTest app = ApplicationTest();
+	app.ExecuteTestA();
+	app.ExecuteTestB();
+	app.ExecuteTestC();
+
+
+	FactoryTeat f = FactoryTeat();
+	f.SetGetIProcessorsTest();
+
+	FileTest ft = FileTest();
+	ft.ReadTest();
+	ft.WriteTest();
+
+	ArgumentATest at = ArgumentATest();
+	at.actionTest();
+	at.addToContainerTest();
+	at.GetFinalResultTest();
+
+	ArgumentBTest bt = ArgumentBTest();
+	bt.actionTest();
+	bt.GetFinalResultTest();
+
+	ArgumentCTest ct = ArgumentCTest();
+	ct.actionTest();
+	ct.GetFinalResultTest();
+
+
+	return 0;
+}
